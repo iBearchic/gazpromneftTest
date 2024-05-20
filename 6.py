eef722d3f08dddcefb5331b2eb81d0b32f37af8c
Maksonio <32421746+iBearchic@users.noreply.github.com>
2024-05-20 16:32:24 +0300

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,67 @@
+# Имеется банковское API возвращающее JSON
+# {
+# 	"Columns": ["key1", "key2", "key3"],
+# 	"Description": "Банковское API каких-то важных документов",
+# 	"RowCount": 2,
+# 	"Rows": [
+# 		["value1", "value2", "value3"],
+# 		["value4", "value5", "value6"]
+# 	]
+# }
+# Основной интерес представляют значения полей "Columns" и "Rows",
+# которые соответственно являются списком названий столбцов и значениями столбцов
+
+# Задание:
+# 	1. Получить JSON из внешнего API
+# 		ендпоинт: GET https://api.gazprombank.ru/very/important/docs?documents_date={"начало дня сегодня в виде таймстемп"}
+# 	2. Валидировать входящий JSON используя модель pydantic
+# 		(из ТЗ известно что поле "key1" имеет тип int, "key2"(datetime), "key3"(str))
+# 	2. Представить данные "Columns" и "Rows" в виде плоского csv-подобного pandas.DataFrame
+# 	3. В полученном DataFrame произвести переименование полей по след. маппингу
+# 		"key1" -> "document_id", "key2" -> "document_dt", "key3" -> "document_name"
+# 	3. Полученный DataFrame обогатить доп. столбцом:
+# 		"load_dt" -> значение "сейчас"(датавремя)
+
+
+import requests
+from datetime import datetime
+from pydantic import BaseModel, ValidationError, conint, constr
+from typing import List
+import pandas as pd
+
+class DocumentRow(BaseModel):
+    key1: conint()
+    key2: datetime
+    key3: constr()
+
+class DocumentData(BaseModel):
+    Columns: List[str]
+    Description: str
+    RowCount: int
+    Rows: List[DocumentRow]
+
+start_of_today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
+
+url = f"https://api.gazprombank.ru/very/important/docs?documents_date={int(start_of_today)}"
+response = requests.get(url)
+data = response.json()
+
+try:
+    document_data = DocumentData(**data)
+except ValidationError as e:
+    print("Ошибка валидации данных:", e)
+    raise
+
+rows = [{col: val for col, val in zip(data["Columns"], row)} for row in data["Rows"]]
+df = pd.DataFrame(rows)
+
+column_mapping = {
+    "key1": "document_id",
+    "key2": "document_dt",
+    "key3": "document_name"
+}
+df.rename(columns=column_mapping, inplace=True)
+
+df["load_dt"] = datetime.now()
+
+print(df)
